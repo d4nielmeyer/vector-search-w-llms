2884c852935ea444548ef7b1d7e010fa41286079
Daniel Meyer <daniel.meyer@vodafone.com>
2023-12-17 07:48:16 +0100

    first attempt

--- app.shell.py
+++ app.shell.py
@@ -1,4 +1,4 @@
-from tiktoken import get_encoding
+from tiktoken import get_encoding, encoding_for_model
 from weaviate_interface import WeaviateClient
 from prompt_templates import question_answering_prompt_series, question_answering_system
 from openai_interface import GPT_Turbo
@@ -26,14 +26,23 @@ st.set_page_config(page_title="Impact Theory",
 ##############
 data_path = './data/impact_theory_data.json'
 ## RETRIEVER
+api_key = os.environ['WEAVIATE_API_KEY']
+url = os.environ['WEAVIATE_ENDPOINT']
+client = WeaviateClient(api_key, url)
+client.display_properties.append('summary')
 
 ## RERANKER
+reranker = ReRanker(model_name='cross-encoder/ms-marco-MiniLM-L-6-v2')
 
 ## LLM 
+model_name = 'gpt-3.5-turbo-0613'
+llm = GPT_Turbo(model_name)
 
 ## ENCODING
+encodings = encoding_for_model(model_name)
 
 ## INDEX NAME
+index_name = 'Impact_theory_minilm_256'
 
 ##############
 #  END CODE  #
@@ -56,85 +65,81 @@ def main():
         st.write('\n\n\n\n\n')
 
         if query:
-            ##############
-            # START CODE #
-            ##############
-
-            st.write('Hmmm...this app does not seem to be working yet.  Please check back later.')
-            if guest:
-                st.write(f'However, it looks like you selected {guest} as a filter.')
             # make hybrid call to weaviate
-            hybrid_response = None
+            where_filter = {
+                "path": ["guest"],
+                "operator": "Equal",
+                "valueText": guest
+            } if guest else None
+
+            hybrid_response = client.hybrid_search(query, index_name, where_filter=where_filter)
+
             # rerank results
-            ranked_response = None
+            ranked_response = reranker.rerank(hybrid_response, query=query, top_k=3)
+
             # validate token count is below threshold
-            # valid_response = validate_token_threshold(ranked_response, 
-                                                    #    question_answering_prompt_series, 
-                                                    #    query=query,
-                                                    #    tokenizer= # variable from ENCODING,
-                                                    #    token_threshold=4000, 
-                                                    #    verbose=True)
-            ##############
-            #  END CODE  #
-            ##############
+            valid_response = validate_token_threshold(ranked_response,
+                                                      question_answering_prompt_series,
+                                                      query=query,
+                                                      tokenizer=encodings,
+                                                      token_threshold=4000,
+                                                      verbose=True)
 
-            # # generate LLM prompt
-            # prompt = generate_prompt_series(base_prompt=question_answering_prompt_series, query=query, results=valid_response)
             
-            # # prep for streaming response
-            # st.subheader("Response from Impact Theory (context)")
-            # with st.spinner('Generating Response...'):
-            #     st.markdown("----")
-            #     #creates container for LLM response
-            #     chat_container, response_box = [], st.empty()
-            #     
-            #     # execute chat call to LLM
-            #                  ##############
-            #                  # START CODE #
-            #                  ##############
-            #     
+            # prep for streaming response
+            st.subheader("Response from Impact Theory (context)")
+            with st.spinner('Generating Response...'):
+                st.markdown("----")
+                # creates container for LLM response
+                chat_container, response_box = [], st.empty()
 
-            #                  ##############
-            #                  #  END CODE  #
-            #                  ##############
-            #         try:
-                          #inserts chat stream from LLM
-            #             with response_box:
-                        #     content = resp.choices[0].delta.content
-                        #     if content:
-                        #         chat_container.append(content)
-                        #         result = "".join(chat_container).strip()
-                        #         st.write(f'{result}')
-                        # except Exception as e:
-                        #     print(e)
-                        #     continue
-            # ##############
-            # # START CODE #
-            # ##############
-            # st.subheader("Search Results")
-            # for i, hit in enumerate(valid_response):
-            #     col1, col2 = st.columns([7, 3], gap='large')
-            #     image = # get thumbnail_url
-            #     episode_url = # get episode_url
-            #     title = # get title
-            #     show_length = # get length
-            #     time_string = # convert show_length to readable time string
-            # ##############
-            # #  END CODE  #
-            # ##############
-            #     with col1:
-            #         st.write( search_result(  i=i, 
-                                                # url=episode_url,
-                                                # guest=hit['guest'],
-                                                # title=title,
-                                                # content=hit['content'], 
-                                                # length=time_string),
-            #                 unsafe_allow_html=True)
-            #         st.write('\n\n')
-            #     with col2:
-            #         # st.write(f"<a href={episode_url} <img src={image} width='200'></a>", 
-            #         #             unsafe_allow_html=True)
-            #         st.image(image, caption=title.split('|')[0], width=200, use_column_width=False)
+                # generate LLM prompt
+                prompt = generate_prompt_series(query=query, results=valid_response)
+
+                # execute chat call to LLM
+                response = llm.get_chat_completion(prompt=prompt,
+                                                   temperature=0,
+                                                   max_tokens=500,
+                                                   stream=True,
+                                                   show_response=True)
+
+                for event in response:
+                    try:
+                        # inserts chat stream from LLM
+                        with response_box:
+                            content = event['choices'][0]['delta']['content']
+
+                            if content:
+                                chat_container.append(content)
+                                result = "".join(chat_container).strip()
+                                st.write(f'{result}')
+                    except Exception as e:
+                        print(e)
+                        continue
+
+            st.subheader("Search Results")
+            for i, hit in enumerate(valid_response):
+                col1, col2 = st.columns([7, 3], gap='large')
+                image = hit['thumbnail_url'] # get thumbnail_url
+                episode_url = hit['episode_url'] # get episode_url
+                title = hit["title"] # get title
+                show_length = hit["length"] # get length
+                time_string = convert_seconds(show_length) # convert show_length to readable time string
+
+                with col1:
+                    st.write(search_result(i=i,
+                                           url=episode_url,
+                                           guest=hit['guest'],
+                                           title=title,
+                                           content=hit['content'],
+                                           length=time_string),
+                             unsafe_allow_html=True)
+                    st.write('\n\n')
+                with col2:
+                    # st.write(f"<a href={episode_url} <img src={image} width='200'></a>",
+                    #             unsafe_allow_html=True)
+                    st.image(image, caption=title.split('|')[0], width=200, use_column_width=False)
+
 
 if __name__ == '__main__':
     main()
